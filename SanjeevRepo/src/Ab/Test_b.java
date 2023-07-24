ee5c3ff87e9c279835b29a5e1b712d06a52ca895
admin <admin@DESKTOP-J7JSP3Q.mshome.net>
2023-07-24 10:42:42 +0530

    Add class test_b.java

--- SanjeevRepo/src/Ab/Test_b.java
+++ SanjeevRepo/src/Ab/Test_b.java
@@ -1,5 +1,42 @@
 package Ab;
 
 public class Test_b {
-
+	int id;
+	String name;
+	double bal;
+	Test_b(int id,String name,double bal){
+		this.id=id;
+		this.name=name;
+		this.bal=bal;
+	}
+	public void display() {
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(bal);
+	}
+	public void withdraw(double amount) {
+		if(amount>0 && amount<=bal) {
+			bal=bal-amount;
+			System.out.println(amount+ "/- withdraw successfully");
+			System.out.println("current balance is "+bal+"/-");
+		}
+		else if(amount==0){
+			System.out.println("invalid input");
+		}
+		else {
+			System.out.println("insufficient balance");
+		}
+	}
+	public void deposit(double amt) {
+		if(amt>0) {
+			bal=bal+amt;
+			System.out.println(amt+"/- successfully deposited");
+			System.out.println("available balance is "+bal);
+		}
+		else {
+			System.out.println("invalid input"+amt);
+		}
+	}
 }
+
+

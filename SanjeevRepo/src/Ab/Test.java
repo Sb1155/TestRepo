82f618c01b8102c741313bd6d8990a279f304a91
admin <admin@DESKTOP-J7JSP3Q.mshome.net>
2023-07-24 10:34:51 +0530

    Same class

--- SanjeevRepo/src/Ab/Test.java
+++ SanjeevRepo/src/Ab/Test.java
@@ -1,5 +1,14 @@
 package Ab;
 
 public class Test {
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println();
+		int b=10;
+		System.out.println(b);
+		
+	}
 
 }
+
+
